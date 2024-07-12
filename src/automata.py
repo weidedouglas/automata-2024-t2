b7d85baaf0cd5325956590d06b2d512abbc94bde
Douglas Weide <douglas.201920299@unilasalle.edu.br>
2024-07-11 21:15:20 -0300

    t2

--- src/automata.py
+++ src/automata.py
@@ -22,7 +22,7 @@ def load_automata(filename):
     q0
     q0 a q1
     q0 b q2
-    q1 a q0
+    q1 a q0 
     q1 b q3
     q2 a q3
     q2 b q0
@@ -34,21 +34,102 @@ def load_automata(filename):
 
     """
 
+    simbolos = [] 
+    estados = []
+    estadosFinais = []
+    estadoInicial = []
+    regrasTransicao = []
+    auxiliar = []
+
     with open(filename, "rt") as arquivo:
-        # processa arquivo...
-        pass
+        # processa arquivo.. .
+        for linha in arquivo:
+            linha = linha.strip()
+            linha = linha.split()
+            auxiliar.append(linha)
+    
+    simbolos = auxiliar[0]
+    estados = auxiliar[1]
+    estadosFinais = auxiliar[2]
+    estadoInicial = auxiliar[3] 
+    regrasTransicao = auxiliar[4:]
+
+    """
+    valida simbolos
+    """
+
+    for valor in simbolos:
+        if not isinstance(valor, str) or not valor.isalpha():
+            raise Exception("simbolos inválidos")
+
+    """
+    valida estados
+    """        
+    for valor in estados:
+        if not valor.startswith("q"):
+            raise Exception("Estados Inválidos")
+        
+    """
+    valida Estados Finais
+    """
+    for valor in estadosFinais:
+        if valor not in estados:
+            raise Exception("O estado final não está contido no conjunto de estados")
+    
+    """
+    valida estado inicial
+    """
+    for valor in estadoInicial:
+        if valor not in estados:
+            raise Exception("O estado inicial não está contido no conjunto de estados")
+        
+    """
+    valida transicoes
+    """
+    for valor in regrasTransicao:
+        if valor[0] not in estados or valor[1] not in simbolos or valor[2] not in estados:
+            raise Exception("Regras de Transição incorretas")
+
+    automatoValido = (simbolos, estados, estadosFinais, estadoInicial, regrasTransicao)  
+    return automatoValido
+
 
 
 def process(automata, words):
+
+    simbolos, estados, estadosFinais, estadoInicial, regrasTransicao = automata
+
     """
     Processa a lista de palavras e retora o resultado.
     
     Os resultados válidos são ACEITA, REJEITA, INVALIDA.
     """
 
+    words = list(words)
+
+    estadoAtual = estadoInicial[0]
+
     for word in words:
-        # tenta reconhecer `word`
+        if word not in simbolos:
+            return print("INVÁLIDA")
+        estadoAtual = Transicao(regrasTransicao, estadoAtual, word)  
+        if estadoAtual == None: 
+            break   
+
+    if estadoAtual is not None:
+        print(f"Estado Final: {estadoAtual}")
+        if estadoAtual in estadosFinais:
+            return("ACEITA")
+        else: 
+            return("REJEITA")
+    else:
+        raise Exception("ERRO ao validar a transicao/estado")
+ 
 
 
-def convert_to_dfa(automata):
-    """Converte um NFA num DFA."""
+def Transicao(regrasTransicao, estadoAtual, word):
+    for transicao in regrasTransicao:
+        if transicao[0] == estadoAtual and transicao[1] == word:
+            return transicao[2]
+    return None
+ 
